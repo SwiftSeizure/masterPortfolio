0b012cd21c9197358790d30bc54d82c7afa7e53b
SwiftSeizure <haidandnelson@gmail.com>
2025-05-18 17:52:17 -0600

    turn off splash

--- src/components/blogCard/BlogCard.js
+++ src/components/blogCard/BlogCard.js
@@ -6,6 +6,10 @@ export default function BlogCard({ blog }) {
     <div>
       <div class="container">
         <div class="square">
+          <img src={blog.image} alt="blog" className="blog-card-image mask" />
+          <div class="blog-card-title">{blog.title}</div>
+          <p className="blog-card-subtitle">{blog.description}</p>
+
           <div>
             <a href={blog.url} target="_" class="button">
               Read More
